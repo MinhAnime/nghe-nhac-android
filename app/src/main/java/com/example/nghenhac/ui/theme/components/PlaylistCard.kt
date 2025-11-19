955c6155172d53b686e4e6e3b608318712dd51e0
minh <minhduong019904@gmail.com>
2025-11-20 02:20:45 +0700

    ok

--- app/src/main/java/com/example/nghenhac/ui/theme/components/PlaylistCard.kt
+++ app/src/main/java/com/example/nghenhac/ui/theme/components/PlaylistCard.kt
@@ -16,12 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.example.nghenhac.R
 import com.example.nghenhac.data.PlaylistSummaryDTO
 
 @Composable
