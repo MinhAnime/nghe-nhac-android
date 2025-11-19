2b2863ce85789035c0bd1129fd200eaa686b5505
minh <minhduong019904@gmail.com>
2025-11-19 23:02:24 +0700

    ok

--- app/src/main/java/com/example/nghenhac/ui/theme/navigation/Screen.kt
+++ app/src/main/java/com/example/nghenhac/ui/theme/navigation/Screen.kt
@@ -11,9 +11,7 @@ sealed class Screen(val route: String, val title: String, val icon: ImageVector)
     object Home : Screen("home", "Trang chủ", Icons.Default.Home)
     object Search : Screen("search", "Tìm kiếm", Icons.Default.Search)
 
-    // Dùng route giả vì nút này mở Dialog chứ không chuyển màn hình
     object AddPlaylist : Screen("add_playlist", "Tạo Playlist", Icons.Default.AddBox)
 
-    // Dùng route giả vì nút này thực hiện đăng xuất
     object Logout : Screen("logout", "Đăng xuất", Icons.Default.ExitToApp)
 }
