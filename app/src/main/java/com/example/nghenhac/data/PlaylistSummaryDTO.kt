8bdbbbbbfd0c821060d5822fee43df69fb288ef2
minh <minhduong019904@gmail.com>
2025-11-20 02:20:51 +0700

    ok

--- app/src/main/java/com/example/nghenhac/data/PlaylistSummaryDTO.kt
+++ app/src/main/java/com/example/nghenhac/data/PlaylistSummaryDTO.kt
@@ -1,8 +1,14 @@
 package com.example.nghenhac.data
 
+import com.google.gson.annotations.SerializedName
+
 data class PlaylistSummaryDTO(
     val id: Long,
     val name: String,
     val ownerUsername: String,
-    val thumbnails: List<String> = emptyList()
-)
+    @SerializedName("thumbnails")
+    private val _thumbnails: List<String>? = null
+){
+    val thumbnails: List<String>
+        get() = _thumbnails ?: emptyList()
+}
